9343ce883fc443b3e9d0253be47dbd7d18d3b69e
Sarah Noshin <tuf46965@temple.edu>
2020-10-13 19:42:59 -0400

    check if email, password, confirm password, user type fields are filled during sign up

--- RestaurantManager/Signup.cs
+++ RestaurantManager/Signup.cs
@@ -40,6 +40,47 @@ namespace RestaurantManager
 
         private async void butSignUp_Click(object sender, EventArgs e)
         {
+            //empty name field
+            if (string.IsNullOrEmpty(txtFullName.Text))
+            {
+                MessageBox.Show("Please enter your name.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtFullName.Focus();
+                return;
+            }
+
+            //empty email
+            if (string.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Please enter your email.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmail.Focus();
+                return;
+            }
+
+            //empty password
+            if (string.IsNullOrEmpty(txtPass.Text))
+            {
+                MessageBox.Show("Please enter your password.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPass.Focus();
+                return;
+            }
+
+            //empty password
+            if (string.IsNullOrEmpty(txtConfirmPass.Text))
+            {
+                MessageBox.Show("Please confirm your password.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtConfirmPass.Focus();
+                return;
+            }
+
+            //empty select user field
+            if (string.IsNullOrEmpty(UserType.Text))
+            {
+                MessageBox.Show("Please select a user.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                UserType.Focus();
+                return;
+            }
+
+
             if (txtPass.Text != txtConfirmPass.Text)
             {
                 if(txtPass.Text!= txtConfirmPass.Text)
@@ -52,6 +93,7 @@ namespace RestaurantManager
 
             IUserRepository repository = new UserRepository();
             bool result = await repository.Insert(new User() { FullName = txtFullName.Text, Password = txtPass.Text, Email = txtEmail.Text, UserType = UserType.Text });
+            
             if (result)
                 MessageBox.Show("You have successfully signed up!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
             else
