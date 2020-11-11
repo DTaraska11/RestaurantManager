643669f09a6eabc83fe69e1db55c649d4f28a2b6
Sarah Noshin <tuf46965@temple.edu>
2020-11-11 02:00:42 -0500

    snack/food/dessert buttons functional

--- RestaurantManager/Drinks.Designer.cs
+++ RestaurantManager/Drinks.Designer.cs
@@ -43,7 +43,7 @@
             // 
             this.textBox1.Font = new System.Drawing.Font("MV Boli", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.textBox1.Location = new System.Drawing.Point(406, 18);
+            this.textBox1.Location = new System.Drawing.Point(470, 18);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(228, 51);
             this.textBox1.TabIndex = 0;
@@ -64,7 +64,7 @@
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(252, 97);
+            this.button1.Location = new System.Drawing.Point(52, 121);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(213, 239);
             this.button1.TabIndex = 1;
@@ -78,7 +78,7 @@
             this.button2.BackColor = System.Drawing.Color.White;
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(587, 97);
+            this.button2.Location = new System.Drawing.Point(587, 122);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(213, 239);
             this.button2.TabIndex = 2;
@@ -92,7 +92,7 @@
             this.button3.BackColor = System.Drawing.Color.White;
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(252, 411);
+            this.button3.Location = new System.Drawing.Point(332, 121);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(213, 240);
             this.button3.TabIndex = 3;
@@ -106,7 +106,7 @@
             this.button4.BackColor = System.Drawing.Color.White;
             this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(587, 411);
+            this.button4.Location = new System.Drawing.Point(839, 120);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(213, 240);
             this.button4.TabIndex = 4;
--- RestaurantManager/Snacks.cs
+++ RestaurantManager/Snacks.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+
 
 namespace RestaurantManager
 {
     public partial class Snacks : UserControl
     {
+        string connectionString = "server=98.115.187.178;port=9005;user=root;password=root;database=Restaurant;"; 
+
         public Snacks()
         {
             InitializeComponent();
@@ -19,7 +25,129 @@ namespace RestaurantManager
 
         private void drinks1_Load(object sender, EventArgs e)
         {
+            OrderItem orderItem = new OrderItem();
 
+            orderItem.GridFill();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                int MenuItemID = 0;
+                string MenuItemName = "fries";
+                int MenuItemCost = 4;
+
+
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("MenuItemAddOrEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_MenuItemID", MenuItemID);
+                mySqlCmd.Parameters.AddWithValue("_MenuItemName", MenuItemName);
+                mySqlCmd.Parameters.AddWithValue("_MenuItemCost", MenuItemCost);
+                mySqlCmd.ExecuteNonQuery();
+
+                DialogResult result = MessageBox.Show("Submitted to Order Successfully!");
+
+
+
+                OrderItem orderItem = new OrderItem();
+
+                orderItem.GridFill();
+
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                int MenuItemID = 0;
+                string MenuItemName = "bread";
+                int MenuItemCost = 2;
+
+
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("MenuItemAddOrEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_MenuItemID", MenuItemID);
+                mySqlCmd.Parameters.AddWithValue("_MenuItemName", MenuItemName);
+                mySqlCmd.Parameters.AddWithValue("_MenuItemCost", MenuItemCost);
+                mySqlCmd.ExecuteNonQuery();
+
+                DialogResult result = MessageBox.Show("Submitted to Order Successfully!");
+
+
+
+                OrderItem orderItem = new OrderItem();
+
+                orderItem.GridFill();
+
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                int MenuItemID = 0;
+                string MenuItemName = "hotdog";
+                int MenuItemCost = 7;
+
+
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("MenuItemAddOrEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_MenuItemID", MenuItemID);
+                mySqlCmd.Parameters.AddWithValue("_MenuItemName", MenuItemName);
+                mySqlCmd.Parameters.AddWithValue("_MenuItemCost", MenuItemCost);
+                mySqlCmd.ExecuteNonQuery();
+
+                DialogResult result = MessageBox.Show("Submitted to Order Successfully!");
+
+
+
+                OrderItem orderItem = new OrderItem();
+
+                orderItem.GridFill();
+
+
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                int MenuItemID = 0;
+                string MenuItemName = "pretzel";
+                int MenuItemCost = 5;
+
+
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("MenuItemAddOrEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_MenuItemID", MenuItemID);
+                mySqlCmd.Parameters.AddWithValue("_MenuItemName", MenuItemName);
+                mySqlCmd.Parameters.AddWithValue("_MenuItemCost", MenuItemCost);
+                mySqlCmd.ExecuteNonQuery();
+                
+                DialogResult result= MessageBox.Show("Submitted to Order Successfully!");
+
+              
+                
+                    OrderItem orderItem = new OrderItem();
+
+                    orderItem.GridFill();
+
+                
+
+            }
         }
     }
 }
